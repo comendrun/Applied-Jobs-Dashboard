db3d081d650354c7e4c7572cd5a4f41bb71ba11d
KamRan <kamran.rouhani.dev@gmail.com>
2022-07-26 09:49:31 +0200

    corrected the style in search page

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -18,10 +18,12 @@ export default function Search() {
   // console.log(data);
 
   return (
-    <div className="text-white card ">
+    <div className="cards-grid overflow-auto desktop:p-4 ">
       {isPending && (
-        <div className="pending-message-home-page fixed top-[50%] left-[50%] desktop:left-[65%] -translate-x-[50%] -translate-y-[50%] p-[50px] z-[1000]">
-          <BounceLoader color="white" size={72} loading />
+        <div className="w-full">
+          <div className="pending-message-home-page fixed top-[50%] left-[50%] desktop:left-[65%] -translate-x-[50%] -translate-y-[50%] p-[50px] z-[1000]">
+            <BounceLoader color="white" size={72} loading />
+          </div>
         </div>
       )}
       {error && <div>{error}</div>}
